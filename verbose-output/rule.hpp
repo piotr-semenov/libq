3134ef3bc0072112661f8c02b6453a9e5594bf0a
Piotr Semenov <piotr.k.semenov@gmail.com>
2011-08-18 10:32:41 -1200

    rule.hpp: interface for tracking rules

---
+++ verbose-output/rule.hpp
@@ -0,0 +1,17 @@
+#include <boost/any.hpp>
+
+#include "./../../Common/functor_info.hpp"
+
+namespace utils { namespace verbose_output {
+    /// @brief rule interface that tracks variable state changes
+    class rule
+        :    public utils::functor_info<bool(void) const>
+    {
+    public:
+        virtual ~rule(){}
+
+        /// @brief tests if current state changing should be logged
+        ///        into the output stream
+        virtual result_type operator(args<0>::type) const = 0;
+    };
+}}
