08f7fb11275829b522fc03fbd3cc71e6d09e4966
Piotr Semenov <piotr.k.semenov@gmail.com>
2011-08-18 13:41:48 -1200

    rule.hpp: enable derived rules to have its own state

--- verbose-output/rule.hpp
+++ verbose-output/rule.hpp
@@ -5,13 +5,13 @@
 namespace utils { namespace verbose_output {
     /// @brief interface for rules tracking variable state changes
     class rule
-        :    public utils::functor_info<bool(boost::any) const>
+        :    public utils::functor_info<bool(boost::any const&)>
     {
     public:
         virtual ~rule(){}
 
         /// @brief tests if current state changing should be logged
         ///        into the output stream
-        virtual result_type operator(args<0>::type) const = 0;
+        virtual result_type operator()(args<0>::type) = 0;
     };
 }}
