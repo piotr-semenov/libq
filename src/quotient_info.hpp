e1373aa06dc628d0ef329ae2d25d9c99fa2a0ebd
Piotr Semenov <piotr.k.semenov@gmail.com>
2012-10-13 14:38:00 +0400

    quotient_info.hpp: refactoring + BOOST_STATIC_ASSERTs were removed

--- src/quotient_info.hpp
+++ src/quotient_info.hpp
@@ -1,13 +1,9 @@
-/// @brief provides fixed-point quotient info
+/// @brief provides info class for division operation result type
 
 #ifndef INC_QUOTIENT_INFO_HPP_
 #define INC_QUOTIENT_INFO_HPP_
 
-#include "./number.hpp"
-
 #include <boost/integer.hpp>
-#include <boost/static_assert.hpp>
-#include <boost/concept_check.hpp>
 
 #include <boost/mpl/if.hpp>
 #include <boost/mpl/eval_if.hpp>
@@ -21,58 +17,49 @@ namespace utils {
         using boost::mpl::eval_if;
     }
 
-    template<typename T, size_t total, size_t fractionals>
+    template<typename storage_type, size_t total, size_t fractionals>
     class number;
 
-    /// @brief meta-class to inference computation primitive quotient type.
-    ///        In case of float-point built-in types
+    /// @brief tool for type inference of the division result with fixed-point and
+    /// floating-point numbers
     template<typename value_type>
     class quotient_info
     {
-        BOOST_STATIC_ASSERT(boost::is_float<value_type>::value);
-
     public:
-        typedef value_type quotient_type;                ///< if type is floating then division is closed operation
+        typedef value_type quotient_type;
         typedef value_type quotient_value_type;
 
-        /// @brief sign meta-property of type value_type
-        struct sign_info
+        struct is_signed
         {
             enum { value = true };
         };
 
-        /// @brief existence of arithmetic operations result for current type
-        struct closing_info
+        struct is_closed
         {
             enum { value = true };
         };
     };
 
-    /// @brief in case of fixed-point types
-    template<typename T, size_t total, size_t fractionals>
-    class quotient_info<number<T, total, fractionals> >
+    /// @brief in case of fixed-point numbers
+    template<typename T, size_t n, size_t f>
+    class quotient_info<number<T, n, f> >
     {
-        BOOST_CONCEPT_ASSERT((boost::IntegerConcept<T>));
-
-        typedef number<T, total, fractionals> operand_type;
+        typedef number<T, n, f> operand_type;
 
     public:
-        /// @brief sign meta-property of type value_type
-        struct sign_info
+        ///< is the type of division a signed type?
+        struct is_signed
         {
             enum { value = std::numeric_limits<T>::is_signed };
-            static size_t const sign_bit = 1u;          // only to increase code readability
         };
 
-        /// @brief existence of arithmetic operations result for current type
-        struct closing_info
+        ///< is type of the division closed under arithmetic operations?
+        struct is_closed
         {
-            typedef typename if_<sign_info, boost::intmax_t, boost::uintmax_t>::type max_type;
+            typedef typename if_<is_signed, boost::intmax_t, boost::uintmax_t>::type max_type;
 
-            // total bits and std::numeric_limits don't count sign bit
-            // if current condition holds then type is closed over arithmetic
-            // operations
-            enum { value = !(total + fractionals <= std::numeric_limits<max_type>::digits) };
+            // total bits and std::numeric_limits do not count sign bit
+            enum { value = !(n + f <= std::numeric_limits<max_type>::digits) };
         };
 
         // logics: 1. if we don't have integral type with enough bits amount then we 
@@ -81,11 +68,11 @@ namespace utils {
         /// @brief integral value type for fixed-point result type of division
         struct value_type_info
         {
-            /// @brief in case if quotient type is non-closed under arithmetic operations
+            ///< in case if quotient type is not closed under arithmetic operations
             struct op
             {
-                typedef typename if_<sign_info, typename boost::int_t<sign_info::sign_bit + total + fractionals>::least,
-                    typename boost::uint_t<total + fractionals>::least>::type type;
+                typedef typename if_<is_signed, typename boost::int_t<is_signed::value + n + f>::least,
+                    typename boost::uint_t<n + f>::least>::type type;
             };
 
             /// @brief in case if quotient type is closed under arithmetic operations
@@ -95,13 +82,14 @@ namespace utils {
             };
         };
 
-        ///< integral type for quotient value type
-        typedef typename eval_if<closing_info, typename value_type_info::cl,
-                                               typename value_type_info::op>::type quotient_value_type;
+        ///< integral value type that is below of fixed-point result type of the
+        /// division
+        typedef typename eval_if<is_closed, typename value_type_info::cl,
+            typename value_type_info::op>::type quotient_value_type;
 
-        ///< fixed-point type for quotient result
-        typedef typename if_<closing_info, operand_type,
-                                           number<quotient_value_type, total + fractionals, 2 * fractionals> >::type quotient_type;
+        ///< fixed-point type for division result
+        typedef typename if_<is_closed, operand_type,
+            number<quotient_value_type, n + f, 2u * f> >::type quotient_type;
     };
 }
 
