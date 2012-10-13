4974900385eab13904acd0a1de213dfd54ca977a
Piotr Semenov <piotr.k.semenov@gmail.com>
2012-10-13 14:09:40 +0400

    product_info.hpp: refactoring + remove BOOST_STATIC_ASSERTs

--- src/product_info.hpp
+++ src/product_info.hpp
@@ -1,9 +1,7 @@
-/// @brief provides fixed-point product info
+/// @brief provides info class for product result type
 
 #ifndef INC_PRODUCT_INFO_HPP_
 #define INC_PRODUCT_INFO_HPP_
-
-#include "./number.hpp"
 
 #include <boost/integer.hpp>
 #include <boost/static_assert.hpp>
@@ -21,71 +19,62 @@ namespace utils {
         using boost::mpl::eval_if;
     }
 
-    template<typename T, size_t total, size_t fractionals>
+    template<typename storage_type, size_t total, size_t fractionals>
     class number;
 
-    /// @brief meta-class to inference computation primitive product type.
-    ///        In case of float-point built-in types
+    /// @brief tool for type inference of the product result with fixed-point and
+    /// floating-point numbers
     template<typename value_type>
     class product_info
     {
-        BOOST_STATIC_ASSERT(boost::is_float<value_type>::value);
-
     public:
-        typedef value_type product_type;                ///< if type is floating then product is closed operation
+        typedef value_type product_type;
         typedef value_type product_value_type;
 
-        /// @brief sign meta-property of type value_type
-        struct sign_info
+        struct is_signed
         {
             enum { value = true };
         };
 
-        /// @brief closeness over arithmetic operations of arithmetic type value_type
-        struct closing_info
+        struct is_closed
         {
             enum { value = true };
         };
     };
 
     /// @brief in case of fixed-point numbers
-    template<typename T, size_t total, size_t fractionals>
-    class product_info<number<T, total, fractionals> >
+    template<typename T, size_t n, size_t f>
+    class product_info<number<T, n, f> >
     {
-        BOOST_CONCEPT_ASSERT((boost::IntegerConcept<T>));
-
-        typedef number<T, total, fractionals> operand_type;
+        typedef number<T, n, f> operand_type;
 
     public:
-        /// @brief sign meta-property of type value_type
-        struct sign_info
+        ///< is the type of summ a signed type?
+        struct is_signed
         {
             enum { value = std::numeric_limits<T>::is_signed };
-            static size_t const sign_bit = 1u;          // only to increase code readability
         };
 
-        /// @brief non-existence of arithmetic operations result for current type
-        struct closing_info
+        ///< is type of the summ closed under arithmetic operations?
+        struct is_closed
         {
-            typedef typename if_<sign_info, boost::intmax_t, boost::uintmax_t>::type max_type;
+            typedef typename if_<is_signed, boost::intmax_t, boost::uintmax_t>::type max_type;
 
-            // total bits and std::numeric_limits don't count sign bit
-            // if current condition holds then type is closed over arithmetic
-            // operations
-            enum { value = !(2 * total <= std::numeric_limits<max_type>::digits) };
+            // total bits and std::numeric_limits do not count sign bit
+            enum { value = !(2u * n + 1u <= std::numeric_limits<max_type>::digits) };
         };
 
-        // logics: 1. if we don't have integral type with enough bits amount then we 
-        //            interpret multiplication as closed operation
-        //         2. boost::int_t template parameter takes into account a sign bit
-        /// @brief integral value type for fixed-point result type of product
+        // brief: 1. if one do not have integral type of enough bits count then
+        //           it has to interpret summation as a closed operation;
+        //        2. boost::int_t template parameter takes into account a sign bit.
+        ///< integral value type below fixed-point number as a product result type
         struct value_type_info
         {
-            /// @brief in case if product type is non-closed under arithmetic operations
+            ///< in case if product type is not closed under arithmetic operations
             struct op
             {
-                typedef typename if_<sign_info, typename boost::int_t<sign_info::sign_bit + 2 * total>::least,
-                    typename boost::uint_t<2 * total>::least>::type type;
+                typedef typename if_<is_signed, typename boost::int_t<2u * n + 1u>::least,
+                    typename boost::uint_t<2u * n>::least>::type type;
             };
 
             /// @brief in case if product type is closed under arithmetic operations
@@ -95,13 +84,13 @@ namespace utils {
             };
         };
 
-        ///< fixed-point value type for product result
-        typedef typename eval_if<closing_info, typename value_type_info::cl,
-                                               typename value_type_info::op>::type product_value_type;
+        ///< integral value type that is below of fixed-point result type of the summ
+        typedef typename eval_if<is_closed, typename value_type_info::cl,
+            typename value_type_info::op>::type product_value_type;
 
         ///< fixed-point type for product result
-        typedef typename if_<closing_info, operand_type,
-                                           number<product_value_type, 2 * total, 2 * fractionals> >::type product_type;
+        typedef typename if_<is_closed, operand_type,
+            number<product_value_type, 2u * n, 2u * f> >::type product_type;
     };
 }
 
