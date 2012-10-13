9227628959429fc1e2d465bab622a701fbde430b
Piotr Semenov <piotr.k.semenov@gmail.com>
2012-10-13 13:57:53 +0400

    diff_info.hpp: refactoring

--- src/diff_info.hpp
+++ src/diff_info.hpp
@@ -3,47 +3,23 @@
 #ifndef INC_DIFFERENCE_INFO_HPP_
 #define INC_DIFFERENCE_INFO_HPP_
 
-#include "./sum_info.hpp"
-
 namespace utils {
     template<typename value_type>
     class sum_info;
 
-    /// @brief meta-class to inference computation primitive subtraction type
+    /// @brief tool for type inference of the subtraction result with fixed-point
+    /// and floating-point numbers
     template<typename value_type>
     class diff_info
+        :    public sum_info<value_type>
     {
     public:
-        /// @brief sign meta-property of type value_type
-        struct sign_info
-        {
-            enum { value = sum_info<value_type>::sign_info::value };
-        };
+        ///< integral value type that is below of fixed-point result type of the
+        /// subtraction (or just a floating-point number)
+        typedef typename sum_info<value_type>::sum_value_type diff_value_type;
 
-        /// @brief existence of arithmetic operations result for current type
-        struct closing_info
-        {
-            enum { value = sum_info<value_type>::closing_info::value };
-        };
-
-        /// @brief integral value type for fixed-point result type of sum
-        struct value_type_info
-        {
-            /// @brief in case if sum type is non-closed under arithmetic operations
-            struct op
-            {
-                typedef typename sum_info<value_type>::value_type_info::op::type type;
-            };
-
-            /// @brief in case if sum type is closed under arithmetic operations
-            struct cl
-            {
-                typedef typename sum_info<value_type>::value_type_info::cl::type type;
-            };
-        };
-
-        typedef typename sum_info<value_type>::sum_type diff_type;              ///< type of difference (in case non-POD-types)
-        typedef typename sum_info<value_type>::sum_value_type diff_value_type;  ///< type of difference value
+        ///< type for subtraction result
+        typedef typename sum_info<value_type>::sum_type diff_type;
     };
 }
 
