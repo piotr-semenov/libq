9ca464a3708cb8f32f80c545157f804834ba99b6
Piotr Semenov <piotr.k.semenov@gmail.com>
2012-10-22 18:39:32 +0400

    overflow_policy.hpp: fast signed/unsigned implementation-independent reducing

--- src/overflow_policy.hpp
+++ src/overflow_policy.hpp
@@ -32,7 +32,7 @@ namespace utils {
                 }
 
                 if (fixed_point::is_signed && boost::intmax_t(val) < fixed_point::bounds::min) {
-                    throw std::underflow_error(m1);
+                    throw std::overflow_error(m1);
                 }
 
                 return static_cast<typename fixed_point::value_type>(val);
@@ -46,10 +46,27 @@ namespace utils {
             /// @brief converts integer to fixed point value type. If it is does not
             /// fit fixed point value type range than it performs by modulus computing.
             template<typename fixed_point, typename T>
-            static inline typename fixed_point checked_convert(T val)
+            static inline typename fixed_point::value_type checked_convert(T val)
             {
-                return static_cast<typename fixed_point>(val %
-                    fixed_point::bounds::max);
+                typedef typename fixed_point::value_type word_type;
+                boost::uintmax_t const max = fixed_point::bounds::max;
+
+                word_type u(0);
+
+                if (fixed_point::is_signed) {
+                    if (val > boost::intmax_t(max)) {
+                        u = static_cast<word_type>((val & max) - max);
+                    }
+                    else {
+                        int const sign_bit(val > 0 ? 1 : -1);
+                        u = sign_bit * static_cast<word_type>((sign_bit * val) & max);
+                    }
+                }
+                else {
+                    u = static_cast<word_type>(val & max);
+                }
+
+                return u;
             }
         };
     };
