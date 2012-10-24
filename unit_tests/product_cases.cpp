3997d0d0bce97f8963082383a0e77fdaa6b0ef86
Piotr Semenov <piotr.k.semenov@gmail.com>
2012-10-24 17:41:37 +0400

    product_cases.cpp: tests for fixed-point multiplication accuracy

--- unit_tests/product_cases.cpp
+++ unit_tests/product_cases.cpp
@@ -1,21 +1,33 @@
 #define BOOST_TEST_STATIC_LINK
 
 #include <boost/test/unit_test.hpp>
-#include <boost/integer.hpp>
+#include <boost/foreach.hpp>
+#include <boost/range/irange.hpp>
 
 #include <limits>
+
 #include <string>
+#include <sstream>
+#include <iomanip>
+#include <stdexcept>
 
 #include "./../../fixed_point_lib/src/number.hpp"
-#include "./../../fixed_point_lib/src/sign_info.hpp"
 
 namespace utils { namespace unit_tests {
-    namespace {
-        using utils::S_number;
-        using utils::U_number;
+    #define iterations 100000
 
-        using utils::sign_info;
+    namespace {
+        using utils::SOU_number;
+        using utils::UOU_number;
+
+        double r(double low, double high)
+        {
+            return low + std::rand() / (double(RAND_MAX) / (high - low));
+        }
     }
+
+    #define fmin(type) double(std::numeric_limits<type>::min())
+    #define fmax(type) double(std::numeric_limits<type>::max())
 
     BOOST_AUTO_TEST_SUITE(Multiplication)
 
@@ -26,58 +38,87 @@ namespace utils { namespace unit_tests {
     ///     inferenced accurately
     BOOST_AUTO_TEST_CASE(commonCheck1)
     {
-        typedef S_number<56, 13>::type type1;
-        typedef U_number<47, 17>::type type2;
+        typedef SOU_number<31, 13>::type type1;
+        typedef UOU_number<29, 17>::type type2;
 
-        type1 const a(-34242.431);
-        type2 const b(1231.321);
+        std::srand(static_cast<unsigned int>(std::time(0)));
 
-        type1::product_type const result = a * b;
-        double const aq(result);
+        BOOST_FOREACH(size_t it, boost::irange<size_t>(0, iterations, 1)) {
+            // shortening range not to take care about rounding errors at the
+            // bounds
+            double u1 = r(fmin(type1) + 1u, fmax(type1) - 1u);
+            double u2 = r(fmin(type2) + 1u, fmax(type2) - 1u);
 
-        std::string const message("multiplication was made with illegal rounding error");
-        BOOST_CHECK_MESSAGE(std::fabs(static_cast<double>(result) + 4.21634E+7) < 1E+3, message);
+            type1 const a(u1);
+            type2 const b(u2);
+            type1::product_type const result = a * b;
+
+            std::stringstream message_stream;
+            message_stream
+                << std::setprecision(10)
+                << u1
+                << " * "
+                << u2
+                << ": multiplication was made with unexpected rounding error";
+
+            BOOST_CHECK_MESSAGE(std::fabs(double(result) - (u1 * u2)) <
+                1E-3 * (std::fabs(u1) + std::fabs(u2)), message_stream.str());
+        }
     }
 
     BOOST_AUTO_TEST_CASE(commonCheck2)
     {
-        typedef U_number<24, 14>::type type1;
-        typedef S_number<32, 16>::type type2;
+        typedef UOU_number<31, 16>::type type1;
+        typedef SOU_number<32, 16>::type type2;
 
-        type1 const a(23.312);
-        type2 const b(10.0343);
+        std::srand(static_cast<unsigned int>(std::time(0)));
 
-        type1::sum_type const result = a * b;
+        BOOST_FOREACH(size_t it, boost::irange<size_t>(0, iterations, 1)) {
+            // shortening range not to take care about rounding errors at the
+            // bounds
+            double u1 = r(fmin(type1) + 1u, fmax(type1) - 1u);
+            double u2 = r(fmin(type1) + 1u, fmax(type1) - 1u);
 
-        std::string const message("multiplication was made with illegal rounding error");
-        BOOST_CHECK_MESSAGE(std::fabs(static_cast<double>(result) - 233.92) < 1E-3, message);
+            type1 const a(u1);
+            type2 const b(u2);
+            type1::product_type const result = a * b;
+
+            std::stringstream message_stream;
+            message_stream
+                << std::setprecision(10)
+                << u1
+                << " * "
+                << u2
+                << ": multiplication was made with unexpected rounding error";
+
+            BOOST_CHECK_MESSAGE(std::fabs(double(result) - (u1 * u2)) <
+                1E-3 * (std::fabs(u1) + std::fabs(u2)), message_stream.str());
+        }
     }
 
-    // SAME-TYPE FIXED-POINT NUMBERS ARE FROM THE MULTIPLICATIVE ABELIAN GROUP
+    // TEST FOR OVERFLOW EVENTS
     //////////////////////////////////////////////////////////////////////////
-    // idea of tests 'unitCheck' and 'commutativityCheck'
-    //      checks if the abelian multiplicative group laws are satisfied
-    BOOST_AUTO_TEST_CASE(unitCheck)
+    BOOST_AUTO_TEST_CASE(exceptionCheck)
     {
-        typedef S_number<15, 9>::type type;
-        type const unit(1.0), a(2.4), b(-3.1), c(0.2);
+        typedef UOU_number<7, 0>::type type;
+        type const a(127), b(2);
 
-        std::string const message("multiplication does not have unit");
-        BOOST_CHECK_MESSAGE((type(a * unit) == type(unit * a)) && (type(unit * a) == type(a)), message);
-        BOOST_CHECK_MESSAGE((type(b * unit) == type(unit * b)) && (type(unit * b) == type(b)), message);
-        BOOST_CHECK_MESSAGE((type(c * unit) == type(unit * c)) && (type(unit * c) == type(c)), message);
-    }
+        try {
+            type const c(a * b);
 
-    BOOST_AUTO_TEST_CASE(commutativityCheck)
-    {
-        typedef S_number<30, 31>::type type;
-        type const a(0.0097), b(0.0465), c(-0.0992);
+            BOOST_FAIL("Positive overflow was not detected");
+        }
+        catch(std::overflow_error e){}
 
-        std::string const message("multiplication does not satisfy commutativity law");
-        BOOST_CHECK_MESSAGE(a * b == b * a, message);
-        BOOST_CHECK_MESSAGE(b * c == c * b, message);
-        BOOST_CHECK_MESSAGE(a * c == c * a, message);
+        typedef UO_number<64, 0>::type type1;
+        type1 const d(12), e(3432);
+
+        d * e;
     }
 
     BOOST_AUTO_TEST_SUITE_END()
+
+    #undef iterations
+    #undef fmin
+    #undef fmax
 }}
