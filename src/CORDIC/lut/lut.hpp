4ece82aca01bc536ccfdf01dfd19c1b2d0c2ee84
Piotr Semenov <piotr.k.semenov@gmail.com>
2012-10-28 18:43:48 +0400

    lut.hpp: singleton class for all pre-defined stuff for CORDIC algorithm

---
+++ src/CORDIC/lut/lut.hpp
@@ -0,0 +1,33 @@
+/// @provides look-up table (LUT) for CORDIC stuff.
+/// @ref see H. Dawid, H. Meyr, "CORDIC Algorithms and Architectures"
+
+#ifndef INC_CORDIC_LUT_HPP_
+#define INC_CORDIC_LUT_HPP_
+
+#include <boost/array.hpp>
+
+namespace utils { namespace cordic {
+    template<size_t n, typename fixed_point>
+    class lut
+        :    public boost::array<fixed_point, n>
+    {
+        typedef boost::array<fixed_point, n> base_class;
+        typedef lut<n, fixed_point> this_class;
+
+    public:
+        static size_t const dim = n;
+        typedef fixed_point fixed_point_type;
+
+        static this_class build_arctan_lut();
+        static double compute_circular_scale(size_t n);
+
+    private:
+        lut(base_class const& dat)
+            :    base_class(dat){}
+    };
+}}
+
+#include "./../../../fixed_point_lib/src/CORDIC/lut/arctan_lut.inl"
+#include "./../../../fixed_point_lib/src/CORDIC/lut/circular_scales.inl"
+
+#endif
