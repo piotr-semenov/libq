bb1ab74f6767061a7052cdfc2d1f01f2f830ef28
Piotr Semenov <piotr.k.semenov@gmail.com>
2012-11-08 13:46:05 +0400

    lut.hpp: refactoring (good names + common includes from inl files)

--- src/CORDIC/lut/lut.hpp
+++ src/CORDIC/lut/lut.hpp
@@ -1,12 +1,18 @@
 /// @provides look-up table (LUT) for CORDIC stuff.
 /// @ref see H. Dawid, H. Meyr, "CORDIC Algorithms and Architectures"
 
-#ifndef INC_CORDIC_LUT_HPP_
-#define INC_CORDIC_LUT_HPP_
+#ifndef INC_CORE_CORDIC_LUT_HPP_
+#define INC_CORE_CORDIC_LUT_HPP_
 
 #include <boost/array.hpp>
 
+#include <boost/foreach.hpp>
+#include <boost/range/irange.hpp>
+
+#include <cmath>
+
 namespace core { namespace cordic {
+    /// @brief look-up table for CORDIC algorithms
     template<size_t n, typename fp>
     class lut
         :    public boost::array<fp, n>
@@ -15,20 +21,35 @@ namespace core { namespace cordic {
         typedef lut<n, fp> this_class;
 
     public:
-        static size_t const dim = n;
-        typedef fp fixed_point_type;
+        static size_t const dim = n; ///< size of lut
+        typedef fp fixed_point_type; ///< type of fixed-point numbers
 
-        static this_class build_arctan_lut();
-        static this_class build_arctanh_lut();
+        /// @brief builds lut for angles in case of CORDIC rotations in circular
+        /// coordinates
+        static this_class circular();
 
-        static this_class build_log2_lut();
-        static this_class build_inv_2roots_lut();
+        /// @brief builds lut for angles in case of CORDIC rotations in hyperbolic
+        /// coordinates. It is not use repeated iterations. All values are unique
+        static this_class hyperbolic_wo_repeated_iterations();
 
-        static double compute_circular_scale(size_t n);
-        static this_class build_circular_scales_lut();
+        /// @brief builds lut for 2^i-powers of 2 in case of arguments from [1, 2]
+        /// for computing exp
+        static this_class pow2();
 
-        static this_class build_hyperbolic_scales_lut();
-        static double compute_hyperbolic_scale(size_t n);
+        /// @brief builds lut for 1/2^i-powers of 2 in case of arguments from [0, 1]
+        /// for computng log2
+        static this_class inv_pow2();
+
+        /// @brief computes scale of n CORDIC-rotations in circular coordinates
+        static double circular_scale(size_t n);
+
+        /// @brief builds lut for scales of n CORDIC rotations in case of circular
+        /// coordinates
+        static this_class circular_scales();
+
+        /// @brief computes scale of n CORDIC-rotations in hyperbolic coordinates.
+        /// It uses repeated iterations for convergence.
+        static double hyperbolic_scale_with_repeated_iterations(size_t n);
 
     private:
         lut(base_class const& dat)
@@ -39,10 +60,10 @@ namespace core { namespace cordic {
 #include "./../../../fixed_point_lib/src/CORDIC/lut/arctan_lut.inl"
 #include "./../../../fixed_point_lib/src/CORDIC/lut/arctanh_lut.inl"
 
-#include "./../../../fixed_point_lib/src/CORDIC/lut/log2_lut.inl"
-#include "./../../../fixed_point_lib/src/CORDIC/lut/inv2roots_lut.inl"
+#include "./../../../fixed_point_lib/src/CORDIC/lut/pow2_lut.inl"
+#include "./../../../fixed_point_lib/src/CORDIC/lut/inv_pow2_lut.inl"
 
 #include "./../../../fixed_point_lib/src/CORDIC/lut/circular_scales.inl"
-#include "./../../../fixed_point_lib/src/CORDIC/lut/hyperbolic_scales.inl"
+#include "./../../../fixed_point_lib/src/CORDIC/lut/hyperbolic_scale.inl"
 
 #endif
