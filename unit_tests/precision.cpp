01f101ff0d37ed13bb2cbf8f43a5d2367a6fcecc
Piotr Semenov <piotr.k.semenov@gmail.com>
2012-11-18 23:17:20 +0400

    precision.cpp: logging + mt19937 random generator

--- unit_tests/precision.cpp
+++ unit_tests/precision.cpp
@@ -3,7 +3,12 @@
 #include <boost/test/unit_test.hpp>
 #include "./../../fixed_point_lib/src/fixed_point.hpp"
 
+#include <boost/random.hpp>
+
+#include <ctime>
+
 #include <iomanip>
+#include <fstream>
 #include <sstream>
 
 #include <exception>
@@ -15,10 +20,12 @@ namespace core { namespace unit_tests {
 
         double r(double low, double high)
         {
+            static boost::mt19937 gen(size_t(std::time(0)));
+            static boost::uniform_01<boost::mt19937> uniform(gen);
             if (low > high) {
                 throw std::exception("illegal range");
             }
-            return low + std::rand() / (double(RAND_MAX) / (high - low));
+            return low + uniform() * (high - low);
         }
     }
 
@@ -31,9 +38,29 @@ namespace core { namespace unit_tests {
 #define U(n, f, name) typedef UOU_fixed_point<n, f>::type name
 #define S(n, f, name) typedef SOU_fixed_point<n, f>::type name
 
+#ifdef LOGGING
+#define INIT_LOGGER(path) \
+    std::fstream stat; \
+    stat.open(path, std::fstream::out | std::fstream::app);
+#define LOG(x) \
+    stat \
+        << std::fixed \
+        << std::setprecision(32u) \
+        << x \
+        << std::endl;
+#define CLOSE_LOGGER() \
+    stat.close();
+#else
+#define INIT_LOGGER(path)
+#define LOG(x)
+#define CLOSE_LOGGER()
+#endif
+
     BOOST_AUTO_TEST_CASE(sum)
     {
-#define PRECISION_TEST(t, error) \
+#define PRECISION_TEST(t, error, path) \
+{ \
+    INIT_LOGGER(path); \
     BOOST_FOREACH(size_t it, boost::irange<size_t>(0, ITERATIONS, 1)) { \
     /* shortening range not to take care about rounding errors at the bounds */ \
         double u1 = r(FMIN(t) + 1u, FMAX(t) - 1u); \
@@ -41,14 +68,16 @@ namespace core { namespace unit_tests {
         \
         t const a(u1); t const b(u2); \
         try { \
-            if (std::fabs(double(a + b) - (u1 + u2)) > 2 * error) { \
+            double const abs_diff(std::fabs(double(a + b) - (u1 + u2))); \
+            LOG(abs_diff); \
+            if (abs_diff > 2 * error) { \
                 std::stringstream message_stream; \
                 message_stream \
                     << std::setprecision(5) \
-                    << u1 \
-                    << " + " \
-                    << u2 \
-                    << ": sum was made with unexpected rounding error"; \
+                    << u1 << " + " << u2 \
+                    << ": summation of Q" \
+                    << t::total - t::fractionals << "." << t::fractionals \
+                    << " was made with unexpected rounding error"; \
                 \
                 BOOST_CHECK_MESSAGE(false, message_stream.str()); \
             } \
@@ -59,19 +88,23 @@ namespace core { namespace unit_tests {
                 << u1 << " " << u2; \
             BOOST_CHECK_MESSAGE(false, message_stream.str()); \
         } \
-    }
+    } \
+    CLOSE_LOGGER(); \
+}
 
-        U(28,13,t1); PRECISION_TEST(t1, 1E-2); // floor(log(10,2^13-1))-1=2
-        S(56,34,t2); PRECISION_TEST(t2, 1E-9); // floor(log(10,2^34-1))-1=9
-        S(61,52,t3); PRECISION_TEST(t3, 1E-14); // floor(log(10,2^52-1))-1=14
-        U(53,23,t4); PRECISION_TEST(t4, 1E-5); // floor(log(10, 2^22-1))-1=5
+        U(28,13,t1); PRECISION_TEST(t1, 1E-2, "./plus_Q15_13"); // floor(log(10,2^13-1))-1=2
+        S(56,34,t2); PRECISION_TEST(t2, 1E-9, "./plus_Q22_34"); // floor(log(10,2^34-1))-1=9
+        S(61,52,t3); PRECISION_TEST(t3, 1E-14, "./plus_Q9_52"); // floor(log(10,2^52-1))-1=14
+        U(53,23,t4); PRECISION_TEST(t4, 1E-5, "./plus_Q30_23"); // floor(log(10, 2^22-1))-1=5
 
 #undef PRECISION_TEST
     }
 
     BOOST_AUTO_TEST_CASE(subtraction)
     {
-#define PRECISION_TEST(t, error) \
+#define PRECISION_TEST(t, error, path) \
+{ \
+    INIT_LOGGER(path); \
     BOOST_FOREACH(size_t it, boost::irange<size_t>(0, ITERATIONS, 1)) { \
     /* shortening range not to take care about rounding errors at the bounds */ \
         double u1 = r(FMIN(t) + 1u, FMAX(t) - 1u); \
@@ -79,14 +112,15 @@ namespace core { namespace unit_tests {
         \
         t const a(u1); t const b(u2); \
         try { \
-            if (std::fabs(double(a - b) - (u1 - u2)) > 2 * error) { \
+            double const abs_diff = std::fabs(double(a - b) - (u1 - u2)); \
+            LOG(abs_diff); \
+            if (abs_diff > 2 * error) { \
                 std::stringstream message_stream; \
                 message_stream \
                     << std::setprecision(5) \
-                    << u1 \
-                    << " - " \
-                    << u2 \
-                    << ": subtraction was made with unexpected rounding error"; \
+                    << u1 << " - " << u2 \
+                    << ": subtraction of Q" << t::total - t::fractionals << "." << t::fractionals \
+                    << " was made with unexpected rounding error"; \
                 \
                 BOOST_CHECK_MESSAGE(false, message_stream.str()); \
             } \
@@ -97,32 +131,39 @@ namespace core { namespace unit_tests {
                 << u1 << " " << u2; \
             BOOST_CHECK_MESSAGE(false, message_stream.str()); \
         } \
-    }
-        S(28,13,t1); PRECISION_TEST(t1, 1E-2); // floor(log(10,2^13-1))-1=2
-        S(56,34,t2); PRECISION_TEST(t2, 1E-9); // floor(log(10,2^34-1))-1=9
-        S(61,52,t3); PRECISION_TEST(t3, 1E-14); // floor(log(10,2^52-1))-1=14
-        S(53,23,t4); PRECISION_TEST(t4, 1E-5); // floor(log(10, 2^22-1))-1=5
+    } \
+    CLOSE_LOGGER(); \
+}
+
+        S(28,13,t1); PRECISION_TEST(t1, 1E-2, "./subtraction_Q15_13"); // floor(log(10,2^13-1))-1=2
+        S(56,34,t2); PRECISION_TEST(t2, 1E-9, "./subtraction_Q22_34"); // floor(log(10,2^34-1))-1=9
+        S(61,52,t3); PRECISION_TEST(t3, 1E-14, "./subtraction_Q9_52"); // floor(log(10,2^52-1))-1=14
+        S(53,23,t4); PRECISION_TEST(t4, 1E-5, "./subtraction_Q30_23"); // floor(log(10, 2^22-1))-1=5
 
 #undef PRECISION_TEST
     }
 
     BOOST_AUTO_TEST_CASE(multiplication)
     {
-#define PRECISION_TEST(t1, t2, error) \
+#define PRECISION_TEST(t1, t2, error, path) \
+{ \
+    INIT_LOGGER(path); \
     BOOST_FOREACH(size_t it, boost::irange<size_t>(0, ITERATIONS, 1)) { \
         double u1 = r(FMIN(t1), FMAX(t1)); \
         double u2 = r(FMIN(t2), FMAX(t2)); \
         \
         try { \
             t1 const a(u1); t2 const b(u2); \
-            if (std::fabs(double(a * b) - (u1 * u2)) > error * (std::fabs(u1) + std::fabs(u2))) { \
+            double const abs_diff = std::fabs(double(a * b) - (u1 * u2)); \
+            LOG(abs_diff); \
+            if (abs_diff > error * (std::fabs(u1) + std::fabs(u2))) { \
                 std::stringstream message_stream; \
                 message_stream \
                     << std::setprecision(5) \
-                    << u1 \
-                    << " * " \
-                    << u2 \
-                    << ": multiplication was made with unexpected rounding error"; \
+                    << u1 << " * " << u2 \
+                    << ": product of Q" << t1::total - t1::fractionals << "." << t1::fractionals \
+                    << " and Q" << t2::total - t2::fractionals << "." << t2::fractionals \
+                    << " was made with unexpected rounding error"; \
                 \
                 BOOST_CHECK_MESSAGE(false, message_stream.str()); \
             } \
@@ -133,18 +174,28 @@ namespace core { namespace unit_tests {
                 << u1 << " " << u2; \
             BOOST_CHECK_MESSAGE(false, message_stream.str()); \
         } \
-    }
+    } \
+    CLOSE_LOGGER(); \
+}
 
-        U(18,13,t1); U(20,15,t2); PRECISION_TEST(t1,t2, 1E-3); // floor(log(10,2^13-1))-1=2
-        S(24,23,t3); S(30,22,t4); PRECISION_TEST(t3,t4, 1E-5); // floor(log(10,2^22-1))-1=5
-        S(30,29,t5); S(33,33,t6); PRECISION_TEST(t5,t6, 1E-7); // floor(log(10,2^22-1))-1=7
+        U(18,13,t1); U(20,15,t2); PRECISION_TEST(t1,t2, 1E-2, "./product_Q5_13_Q5_15"); // floor(log(10,2^13-1))-1=2
+        S(24,23,t3); S(30,22,t4); PRECISION_TEST(t3,t4, 1E-5, "./product_Q1_23_Q8_22"); // floor(log(10,2^22-1))-1=5
+        S(30,29,t5); S(33,33,t6); PRECISION_TEST(t5,t6, 1E-7, "./product_Q1_29_Q0_33"); // floor(log(10,2^29-1))-1=7
+
+        PRECISION_TEST(t1,t1, 1E-2, "./product_Q5_13");
+        PRECISION_TEST(t2,t2, 1E-3, "./product_Q5_15");
+        PRECISION_TEST(t3,t3, 1E-5, "./product_Q1_23");
+        PRECISION_TEST(t4,t4, 1E-5, "./product_Q8_22");
+        PRECISION_TEST(t5,t5, 1E-7, "./product_Q1_29");
 
 #undef PRECISION_TEST
     }
 
     BOOST_AUTO_TEST_CASE(division)
     {
-#define PRECISION_TEST(t1, t2, error) \
+#define PRECISION_TEST(t1, t2, error, path) \
+{ \
+    INIT_LOGGER(path); \
     BOOST_FOREACH(size_t it, boost::irange<size_t>(0, ITERATIONS, 1)) { \
         double u1 = r(FMIN(t1), FMAX(t1)); \
         double u2 = r(FMIN(t2), FMAX(t2)); \
@@ -154,16 +205,19 @@ namespace core { namespace unit_tests {
         \
         try { \
             t1 const a(u1); t2 const b(u2); \
+            double const abs_diff = std::fabs(double(a / b) - (u1 / u2)); \
+            LOG(abs_diff); \
+            \
             double const u1a(std::fabs(u1)); double const u2a(std::fabs(u2)); \
             /* supremum of the error */ \
-            if (std::fabs(double(a / b) - (u1 / u2)) > error * ((u1a + u2a) / (u2a * std::fabs(u2a - error)))) { \
+            if (abs_diff > error * ((u1a + u2a) / (u2a * std::fabs(u2a - error)))) { \
                 std::stringstream message_stream; \
                 message_stream \
                     << std::setprecision(5) \
-                    << u1 \
-                    << " / " \
-                    << u2 \
-                    << ": division was made with unexpected rounding error"; \
+                    << u1 << " / " << u2 \
+                    << ": division of Q" << t1::total - t1::fractionals << "." << t1::fractionals \
+                    << " by Q" << t2::total - t2::fractionals << "." << t2::fractionals \
+                    << " was made with unexpected rounding error"; \
                 \
                 BOOST_CHECK_MESSAGE(false, message_stream.str()); \
             } \
@@ -174,22 +228,30 @@ namespace core { namespace unit_tests {
                 << u1 << " " << u2; \
             BOOST_CHECK_MESSAGE(false, message_stream.str()); \
         } \
-    }
+    } \
+    CLOSE_LOGGER(); \
+}
 
-        U(18,13,t1); U(20,15,t2); PRECISION_TEST(t1,t2, 1E-2); // floor(log(10,2^13-1))-1=2
-        S(24,23,t3); S(30,22,t4); PRECISION_TEST(t3,t4, 1E-5); // floor(log(10,2^22-1))-1=5
-        S(30,29,t5); S(33,33,t6); PRECISION_TEST(t5,t6, 1E-7); // floor(log(10,2^22-1))-1=7
+        U(18,13,t1); U(20,15,t2); PRECISION_TEST(t1,t2, 1E-2, "division_Q5_13_Q5_15"); // floor(log(10,2^13-1))-1=2
+        S(24,23,t3); S(30,22,t4); PRECISION_TEST(t3,t4, 1E-5, "division_Q1_23_Q8_22"); // floor(log(10,2^22-1))-1=5
+        S(30,29,t5); S(33,33,t6); PRECISION_TEST(t5,t6, 1E-7, "division_Q1_29_Q0_33"); // floor(log(10,2^22-1))-1=7
+
+        PRECISION_TEST(t1,t1, 1E-2, "./division_Q5_13");
+        PRECISION_TEST(t2,t2, 1E-3, "./division_Q5_15");
+        PRECISION_TEST(t3,t3, 1E-5, "./division_Q1_23");
+        PRECISION_TEST(t4,t4, 1E-5, "./division_Q8_22");
+        PRECISION_TEST(t5,t5, 1E-7, "./division_Q1_29");
 
 #undef PRECISION_TEST
     }
 
-    BOOST_AUTO_TEST_CASE(log)
+    BOOST_AUTO_TEST_CASE(log_sqrt)
     {
 #define PRECISION_TEST(t, low, high, f, error) \
     BOOST_FOREACH(size_t i, boost::irange<size_t>(0, ITERATIONS, 1)) \
     { \
         double const u(r(low, high)); \
-        t::##f##_type const x = std::##f##(fp(u)); \
+        t::##f##_type const x = std::##f##(t(u)); \
         \
         if (std::fabs(double(x) - std::##f##(u)) > error) { \
             std::stringstream message_stream; \
@@ -208,7 +270,16 @@ namespace core { namespace unit_tests {
         } \
     }
 
-    //U(23,17,t1); PRECISION_TEST(t, 
+    // log
+    U(23,17,t1); /*PRECISION_TEST(t1, 1.0, FMAX(t1), log, 1E-3);*/
+    //U(32,16,t2); PRECISION_TEST(t2, 1.0, FMAX(t2), log, 1E-3);
+    //S(43,20,t3); PRECISION_TEST(t3, 1E-2, FMAX(t3), log, 1E-4);
+    //S(50,13,t4); PRECISION_TEST(t4, 1E-2, FMAX(t4), log, 1E-2);
+
+    // sqrt
+    //PRECISION_TEST(t1, FMIN(t1), FMAX(t1), sqrt, 1E-2);
+    U(32,30,t2); PRECISION_TEST(t2, FMIN(t2), FMAX(t2), sqrt, 1E-4);
+    U(40,20,t3); PRECISION_TEST(t3, FMIN(t3), FMAX(t3), sqrt, 1E-1);
 
 #undef PRECISION_TEST
     }
