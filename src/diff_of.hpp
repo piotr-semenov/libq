7d9f79eec8de1afdaa04f6f66e78542e2726af3a
Piotr Semenov <piotr.k.semenov@gmail.com>
2012-11-23 13:39:39 +0400

    rename to achieve great uniformity

---
+++ src/diff_of.hpp
@@ -0,0 +1,18 @@
+/// @brief provides info class about subtraction result type
+
+#ifndef INC_CORE_DIFF_OF_HPP_
+#define INC_CORE_DIFF_OF_HPP_
+
+namespace core {
+    template<typename word_type>
+    class sum_of;
+
+    /// @brief tool for type inference of the subtraction result with fixed-point
+    /// and floating-point numbers
+    template<typename word_type>
+    class diff_of
+        :    public sum_of<word_type>
+    {};
+}
+
+#endif
