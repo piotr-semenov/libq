eff8f69e146382a0edf9e3692429682f74753c1b
Piotr Semenov <piotr.k.semenov@gmail.com>
2012-12-03 12:49:59 +0400

    as_native_proxy.hpp: was almost rewritten to provide all operators suitable for integrals

--- include/as_native_proxy.hpp
+++ include/as_native_proxy.hpp
@@ -1,7 +1,9 @@
 /// @brief provides a proxy class to deal with integer below fixed-point number
 
-#include <boost/operators.hpp>
-#include <boost/numeric/conversion/cast.hpp>
+#ifndef INC_CORE_AS_NATIVE_PROXY_HPP_
+#define INC_CORE_AS_NATIVE_PROXY_HPP_
+
+#include <boost/integer/integer_mask.hpp>
 
 namespace core {
     template<typename T, size_t n, size_t f, class op, class up>
@@ -10,36 +12,46 @@ namespace core {
     /// @brief proxy class for dealing with fixed-point numbers as with integers
     template<typename T, size_t n, size_t f, class op, class up>
     class as_native_proxy
-        :    boost::operators<as_native_proxy<T, n, f, op, up> >,
-             boost::operators2<as_native_proxy<T, n, f, op, up>, T>,
-             boost::shiftable<as_native_proxy<T, n, f, op, up> >,
-             boost::shiftable2<as_native_proxy<T, n, f, op, up>, T>
     {
         typedef as_native_proxy<T, n, f, op, up> this_class;
         typedef fixed_point<T, n, f, op, up> fixed_point_class;
 
     public:
-        template<typename T>
-        operator T() const
+        template<typename U>
+        operator U() const
         {
-            BOOST_STATIC_ASSERT((boost::is_arithmetic<T>::value));
+            BOOST_STATIC_ASSERT((boost::is_arithmetic<U>::value));
 
-            return T(this->value()); 
+            return U(this->value()); 
         }
 
         /// @brief copy operator
-        template<typename T>
-        void operator =(T val){ this->m_value = val; }
+        template<typename U>
+        void operator =(U val)
+        {
+            BOOST_STATIC_ASSERT((boost::is_integral<U>::value));
 
-        /// @brief less operator. It is the same as one for native integral types
-        /// is
-        bool operator <(this_class const& x) const{ return x.value() < this->value(); }
-        bool operator <(T x) const{ return x < this->value(); }
+            this->m_value = val;
+        }
 
-        /// @brief equality comparison operator. It is the same as one for native
-        /// integral types is
-        bool operator ==(this_class const& x) const{ return x.value() == this->value(); }
-        bool operator ==(T x) const{ return x == this->value(); }
+        // motivation: boost ordered operators from operators.hpp are not the option
+        // because it need the copy operator
+#define OPERATOR(f) \
+    template<typename U> \
+    bool operator ##f(U const& x) const \
+    { \
+        BOOST_STATIC_ASSERT((boost::is_integral<U>::value)); \
+        return x ##f this->value(); \
+    } \
+    template<> \
+    bool operator ##f(this_class const& x) const{ return x.value() ##f this->value(); }
+
+        OPERATOR(<);
+        OPERATOR(>);
+        OPERATOR(==);
+        OPERATOR(<=);
+        OPERATOR(>=);
+#undef OPERATOR
 
 #define CHECKED_ARITHMETICS(f, val) \
     fixed_point_class::handle_underflow( \
@@ -47,115 +59,32 @@ namespace core {
         fixed_point_class::handle_overflow(this->value() f val, op()), \
         up() \
     )
+#define OPERATOR(f) \
+    template<typename U> \
+    this_class& operator ##f(U const& x) \
+    { \
+        CHECKED_ARITHMETICS(##f, x); \
+        return *this; \
+    } \
+    template<> \
+    this_class& operator ##f(this_class const& x) \
+    { \
+        CHECKED_ARITHMETICS(##f, x.value()); \
+        return *this; \
+    }
 
-        /// @brief addition operator. It checks if result fits the bounds drawn
-        /// from n bits of length
-        this_class& operator +=(this_class const& x)
-        {
-            CHECKED_ARITHMETICS(+=, x.value());
-
-            return *this;
-        }
-        this_class& operator +=(T x)
-        {
-            CHECKED_ARITHMETICS(+=, x);
-
-            return *this;
-        }
-
-        /// @brief subtraction operator. It checks if result fits the bounds drawn
-        /// from n bits of length (signed/unsigned).
-        this_class& operator -=(this_class const& x)
-        {
-            CHECKED_ARITHMETICS(-=, x.value());
-
-            return *this;
-        }
-        this_class& operator -=(T x)
-        {
-            CHECKED_ARITHMETICS(-=, x);
-
-            return *this;
-        }
-
-        /// @brief multiplication operator. It checks if result fits the bounds
-        /// drawn from n bits of length.
-        this_class& operator *=(this_class const& x)
-        {
-            CHECKED_ARITHMETICS(*=, x.value());
-
-            return *this;
-        }
-        this_class& operator *=(T x)
-        {
-            CHECKED_ARITHMETICS(*=, x);
-
-            return *this;
-        }
-
-        /// @brief division operator. It checks if result fits the bounds drawn
-        /// from n bits of length.
-        this_class& operator /=(this_class const& x)
-        {
-            CHECKED_ARITHMETICS(/=, x.value());
-
-            return *this;
-        }
-        this_class& operator /=(T x)
-        {
-            CHECKED_ARITHMETICS(/=, x);
-
-            return *this;
-        }
+        OPERATOR(+=);
+        OPERATOR(-=);
+        OPERATOR(*=);
+        OPERATOR(/=);
+        OPERATOR(%=);
+        OPERATOR(^=);
+        OPERATOR(&=);
+        OPERATOR(|=);
+        OPERATOR(<<=);
+        OPERATOR(>>=);
+#undef OPERATOR
 #undef CHECKED_ARITHMETICS
-
-        /// @brief mod operator. It is the same as one for native types is.
-        this_class& operator %=(this_class const& x)
-        {
-            this->value() %= x.value();
-
-            return *this;
-        }
-        this_class& operator %=(T x){ this->value() %= x; return *this; }
-
-        /// @brief xor operator. It checks if result fits the bounds drawn from
-        /// n bits of length
-        this_class& operator ^=(this_class const& x)
-        {
-            this->value() ^= x.value();
-
-            return *this;
-        }
-        this_class& operator ^=(T x)
-        {
-            this->value() ^= boost::numeric_cast<fixed_point_class::word_type>(x);
-
-            return *this;
-        }
-
-        /// @brief and operator. It is the same as one for native types is.
-        this_class& operator &=(this_class const& x)
-        {
-            this->value() &= x.value();
-            
-            return *this;
-        }
-        this_class& operator &=(T x){ this->value() &= x; return *this; }
-
-        /// @brief or operator. It checks if result fits the bounds drawn from
-        /// n bits of length
-        this_class& operator |=(this_class const& x)
-        {
-            this->value() |= x.value();
-
-            return *this;
-        }
-        this_class& operator |=(T x)
-        {
-            this->value() |= boost::numeric_cast<fixed_point_class::word_type>(x);
-
-            return *this;
-        }
 
         /// @brief increment operator. It checks if result fits the bounds drawn
         /// from n bits of length.
@@ -175,32 +104,33 @@ namespace core {
             return *this;
         }
 
-        /// @brief left shift operator. It checks if result fits the bounds drawn
-        /// from n bits of length.
-        this_class& operator <<=(this_class const& x)
-        {
-            fixed_point_class::handle_overflow(this->value() <<= x.value(), op());
+#define OPERATOR(f) \
+    template<typename U> \
+    friend T operator ##f(this_class const& x, U const& y) \
+    { \
+        BOOST_STATIC_ASSERT((boost::is_integral<U>::value)); \
+        return x.value() + y; \
+    } \
+    template<typename U> \
+    friend U operator ##f(U const& x, this_class const& y) \
+    { \
+        BOOST_STATIC_ASSERT((boost::is_integral<U>::value)); \
+        return y.value() + x; \
+    }
 
-            return *this;
-        }
-        this_class& operator <<=(T x)
-        {
-            fixed_point_class::handle_overflow(this->value() <<= x, op());
+        OPERATOR(+);
+        OPERATOR(-);
+        OPERATOR(*);
+        OPERATOR(/);
+        OPERATOR(%);
+        OPERATOR(^);
+        OPERATOR(|);
+        OPERATOR(&);
+#undef OPERATOR
 
-            return *this;
-        }
-
-        /// @brief right shift operator. It is the same as one for native integers
-        /// is.
-        this_class& operator >>=(this_class const& x)
+        this_class& operator ~()
         {
-            this->value() >>= x.value();
-
-            return *this;
-        }
-        this_class& operator >>=(T x)
-        {
-            this->value() >>= x;
+            this->m_value ^= boost::low_bits_mask_t<n>::value;
 
             return *this;
         }
@@ -212,6 +142,7 @@ namespace core {
 
         as_native_proxy(fixed_point_class& x)
             :    m_value(x.m_value){};
+        as_native_proxy(this_class const&){};
 
         template<typename T1, size_t n1, size_t f1, class op1, class up1>
         friend as_native_proxy<T1, n1, f1, op1, up1> as_native(fixed_point<T1, n1, f1, op1, up1>&);
@@ -220,3 +151,5 @@ namespace core {
         friend class fixed_point;
     };
 }
+
+#endif
