e97083eebc59c450dc92bc37da65f77191c4cea4
Piotr Semenov <piotr.k.semenov@gmail.com>
2016-04-29 19:05:33 +0300

    [SNAPSHOT] Resolves ~20 cpplint errors

--- libq/type_promotion.hpp
+++ libq/type_promotion.hpp
@@ -1,6 +1,6 @@
 // type_promotion.hpp
 //
-// Copyright (c) 2014-2015 Piotr K. Semenov (piotr.k.semenov at gmail dot com)
+// Copyright (c) 2016 Piotr K. Semenov (piotr.k.semenov at gmail dot com)
 // Distributed under the New BSD License. (See accompanying file LICENSE)
 
 /*!
@@ -12,8 +12,11 @@
 #ifndef INC_LIBQ_TYPE_PROMOTION_HPP_
 #define INC_LIBQ_TYPE_PROMOTION_HPP_
 
-#include "boost/mpl/eval_if.hpp"
-#include "boost/utility/enable_if.hpp"
+#include <boost/integer.hpp>
+#include <boost/mpl/eval_if.hpp>
+
+#include <limits>
+#include <type_traits>
 
 namespace libq {
 namespace details {
@@ -25,19 +28,30 @@ namespace details {
  \tparam delta_f
 */
 template<typename T, std::size_t delta_n, std::size_t delta_f, int delte_e>
-class type_promotion_base
-{
-public:
+class type_promotion_base {
+  public:
     using promoted_type = T;
 };
 
-template<typename _T, std::size_t _n, std::size_t _f, int _e, class _op, class _up, std::size_t delta_n, std::size_t delta_f, int delta_e>
-class type_promotion_base<libq::fixed_point<_T, _n, _f, _e, _op, _up>, delta_n, delta_f, delta_e>
-{
-    using Q = libq::fixed_point<_T, _n, _f, _e, _op, _up>;
+template<typename T,
+        std::size_t n,
+        std::size_t f,
+        int e,
+        class op,
+        class up,
+        std::size_t delta_n,
+        std::size_t delta_f,
+        int delta_e>
+class type_promotion_base<libq::fixed_point<T, n, f, e, op, up>,
+                          delta_n,
+                          delta_f,
+                          delta_e> {
+    using Q = libq::fixed_point<T, n, f, e, op, up>;
     using this_class = type_promotion_base<Q, delta_n, delta_f, delta_e>;
 
-    using max_type = typename std::conditional<Q::is_signed, std::intmax_t, std::uintmax_t>::type;
+    using max_type = typename std::conditional<Q::is_signed,
+                                               std::intmax_t,
+                                               std::uintmax_t>::type;
     enum: std::size_t {
         sign_bit = static_cast<std::size_t>(Q::is_signed),
 
@@ -49,41 +63,34 @@ class type_promotion_base<libq::fixed_point<_T, _n, _f, _e, _op, _up>, delta_n, 
     };
 
     // simple "type" wrapper for lazy instantiation of its "internal" type
-    struct storage_type_promotion_traits
-    {
-        using type = typename std::conditional<
-            Q::is_signed
-            , typename boost::int_t<(n + delta_n) + (f + delta_f) + this_class::sign_bit>::least // boost::int_t takes a sign bit into account
-            , typename boost::uint_t<(n + delta_n) + (f + delta_f)>::least
-        >::type;
+    struct storage_type_promotion_traits {
+        // Note, boost::int_t takes a sign bit into account
+        using type = typename std::conditional<Q::is_signed,
+                                               typename boost::int_t<(n + delta_n) + (f + delta_f) + this_class::sign_bit>::least,  // NOLINT
+                                               typename boost::uint_t<(n + delta_n) + (f + delta_f)>::least>::type;  // NOLINT
     };
-    struct storage_type_default_traits
-    {
+    struct storage_type_default_traits {
         using type = typename Q::storage_type;
     };
 
-public:
-
+  public:
     enum: bool {
-        is_expandable = ((n + delta_n) + (f + delta_f) <= std::numeric_limits<max_type>::digits),
+        is_expandable = ((n + delta_n) + (f + delta_f) <=
+                            std::numeric_limits<max_type>::digits)
     };
 
     /*!
      \brief
     */
-    using promoted_storage_type = typename boost::mpl::eval_if_c<
-        this_class::is_expandable
-        , typename this_class::storage_type_promotion_traits
-        , typename this_class::storage_type_default_traits
-    >::type;
+    using promoted_storage_type =
+        typename boost::mpl::eval_if_c<this_class::is_expandable,
+                                       typename this_class::storage_type_promotion_traits,  // NOLINT
+                                       typename this_class::storage_type_default_traits>::type;  // NOLINT
 
-    using promoted_type = typename std::conditional<
-        this_class::is_expandable
-        , libq::fixed_point<promoted_storage_type, n + delta_n, f + delta_f, e + delta_e, typename Q::overflow_policy, typename Q::underflow_policy>
-        , libq::fixed_point<promoted_storage_type, n, f, e, typename Q::overflow_policy, typename Q::underflow_policy>
-    >::type;
+    using promoted_type = typename std::conditional<this_class::is_expandable,
+        libq::fixed_point<promoted_storage_type, n + delta_n, f + delta_f, e + delta_e, typename Q::overflow_policy, typename Q::underflow_policy>,  // NOLINT
+        libq::fixed_point<promoted_storage_type, n, f, e, typename Q::overflow_policy, typename Q::underflow_policy> >::type;  // NOLINT
 };
-
-} // details
-} // libq
-#endif // INC_LIBQ_TYPE_PROMOTION_HPP_
+}  // namespace details
+}  // namespace libq
+#endif  // INC_LIBQ_TYPE_PROMOTION_HPP_
